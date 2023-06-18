05848b5f0c2d9cf83a20f542e63d26af06912492
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-06-18 16:39:57 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -11,12 +12,15 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-player.on('timeupdate', function (data) {
-  const currentTime = data.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
 if (savedTime) {
-  player.setCurrentTime(parseFloat(currentTime));
+  player.setCurrentTime(parseFloat(savedTime));
 }
